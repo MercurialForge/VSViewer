0fa3f008e7df62082e62cc42b57a35e8aabc268b
Oliver Barraza <oliver@mercurialforge.com>
2015-08-02 20:22:43 -0700

    base windows shell commit. Binary reader added and simple read test from main window.

---
+++ VSViewer/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VSViewer.ViewModels;
+
+namespace VSViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)DataContext).Read_Click();
+        }
+    }
+}
---
+++ VSViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameFormatReader.Common;
+using System.IO;
+
+namespace VSViewer.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            Console.WriteLine("hello from the view model");
+        }
+
+        public void Read_Click()
+        {
+            string file = "";
+
+            // open file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                file = openFileDialog.FileName;
+            }
+
+            // load file into memory stream
+            FileStream fileStream = File.OpenRead(file);
+            MemoryStream memStream = new MemoryStream();
+            memStream.SetLength(fileStream.Length);
+            fileStream.Read(memStream.GetBuffer(), 0, (int)fileStream.Length);
+
+            // create endian reader
+            EndianBinaryReader reader = new EndianBinaryReader(memStream, Endian.Big);
+
+            //being reader
+
+            // Magic H01 + 0x00
+            reader.SkipInt32();
+            Console.WriteLine(" ");
+            Console.WriteLine((int)reader.ReadByte());
+            Console.WriteLine((int)reader.ReadByte());
+            Console.WriteLine(reader.ReadUInt16());
+
+            fileStream.Close();
+            memStream.Close();
+            reader.Close();
+
+        }
+    }
+}
